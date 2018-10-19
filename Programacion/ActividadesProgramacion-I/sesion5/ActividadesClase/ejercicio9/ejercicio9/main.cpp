212ac829c853ddada34e6fed30451835664cb789
gsosad <43112434+gsosad@users.noreply.github.com>
2018-10-19 16:35:12 +0200

    Update main.cpp

--- Programacion/ActividadesProgramacion-I/sesion5/ActividadesClase/ejercicio9/ejercicio9/main.cpp
+++ Programacion/ActividadesProgramacion-I/sesion5/ActividadesClase/ejercicio9/ejercicio9/main.cpp
@@ -2,13 +2,11 @@
 
 using namespace std;
 
-
-
 int main()
 {
     int num;
     int max{0};
-    int min{1000000000};
+    int min{1000000000};//Se le asigna un numero enorme para asegurarse que no se introduzca un numero mayor
     cout << "Introduce numeros enteros e introduce un 0 cuando quieras parar y te dire cual es el maximo y cual el minimo" << endl;
     while(num != 0){
     cout << "Introduce a number: ";
